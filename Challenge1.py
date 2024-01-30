f9528efc3e1a4e6b084ba079d7919cf156af7798
smondragon3910 <smondragon3910@student.pps.net>
2024-01-30 15:26:32 -0800

    Finished Challenges 1-2 on Packet 1 :)

---
+++ Challenge1.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you?"))
+days = age * 365
+print("You are {} days old.".format(days))
+decades = age / 10
+weeks = age * 52
+minutes = age * 525600
+print("You are {} days old, {} decades old, {} weeks old, and {} minutes old. You're old!".format(days, decades, weeks, minutes))
---
+++ Challenge3.py
@@ -0,0 +1,14 @@
+Length = input("Enter a length: ")
+Width = input("Enter a width: ")
+Cost = input("Enter the cost: ")
+
+Cost = Length * 5
+
+print("Area: ")
+print("Perimeter: ")
+print("Total cost: ")
+
+print("Area: {}.".format(Length))
+decades = age / 10
+weeks = age * 52
+minutes = age * 525600
