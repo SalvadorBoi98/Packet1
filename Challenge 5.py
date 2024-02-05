3a127c3e2b98fa2a3194034982b3fe27b3f61450
smondragon3910 <smondragon3910@student.pps.net>
2024-02-05 14:43:56 -0800

    Completed

--- Challenge 5.py
+++ Challenge 5.py
@@ -2,4 +2,4 @@ FirstCut = float(input("Enter the first cut of your triangle: "))
 SecondCut = float(input("Enter the second cut of your triangle: "))
 area_of_triangle = (FirstCut * SecondCut) / 2
 
-print(f"Area of triangle is: (area_of_triangle")
+print("Area of the triangle is "+format(area_of_triangle))
